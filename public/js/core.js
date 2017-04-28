1d76df2a3081abb9c0772ef85f3c17f4f8f48982
dober <doberste8@gmail.com>
2017-04-28 04:21:10 +0000

    began creating html layout and linking database info into it

--- public/js/core.js
+++ public/js/core.js
@@ -3,26 +3,19 @@ var nclbi = angular.module('nclbi', []);
 
 function mainController($scope, $http) {
     $scope.formData = {};
+    $scope.aName = "Name";
+    $scope.bName = "Name";
+    var draftData = {};
 
     //get random from item from list and show it
     $scope.showNewItem = function() {
-        $http.get('/api/list')
+        $http.get('/api/draft_list')
             .success(function(data) {
-                if (!data[0]) {
-                    $http.put('/api/list/all', {
-                        "count": 1
-                    });
-                    $scope.showNewItem();
-                }
-                else {
                     shuffle(data);
-                    $http.put('/api/list', {
-                        "id": data[0].id,
-                        "count": 0
-                    });
-                    $scope.item = data[0].descr;
-                }
-            })
+                    draftData = data;
+                    $scope.aName = data[0].first_name + " " + data[0].last_name;
+                    $scope.bName = data[1].first_name + " " + data[1].last_name;
+                })
             .error(function(data) {
                 console.log('Error: ' + data);
             });
@@ -33,6 +26,20 @@ function mainController($scope, $http) {
             .success(function(data) {
                 $scope.formData = {};
                 $scope.item = data.descr;
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+    
+    //add match to database when user clicks the winner
+        $scope.addMatch = function(a,b) {
+            var match = {event_id: draftData[a].event_id, username: "Steve", player_w_id: draftData[a].id, player_l_id: draftData[b].id, confidence: 4};
+            console.log(match);
+        $http.post('/api/draft_matches/', match)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.item = "posted";
             })
             .error(function(data) {
                 console.log('Error: ' + data);
